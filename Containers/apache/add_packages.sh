213e83d4f6a23e4b8eca3836812d8d1b0e10d8c1
Jordi Bruguera <37143669+jorx-dev@users.noreply.github.com>
2019-01-28 11:23:33 +0100

    Update add_packages.sh

--- Containers/apache/add_packages.sh
+++ Containers/apache/add_packages.sh
@@ -4,3 +4,5 @@ set -e
 zypper --non-interactive --gpg-auto-import-keys ref
 
 zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python python-xml apache2 apache2-prefork
+
+zypper --non-iteractive patch
